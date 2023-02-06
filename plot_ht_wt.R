b2f202c7bd63f6d6fcf56215ae263474d2062b22
DarthDanAmesh <danielamemba@gmail.com>
2023-02-06 06:39:01 -0800

    added quantiles visualization in histogram plots

--- plot_ht_wt.R
+++ plot_ht_wt.R
@@ -21,6 +21,27 @@ quantile(ht_data, probs=c(0.60,0.80,0.90))
 #if there is na values in the data, set argument na.rm=TRUE in the quantile function.
 quantile(wt_data, c(0.25,0.45,0.65,0.85), na.rm=TRUE)
 
+#visualize or plot the quantiles for ht(y-axis)
+htplot_quant<- quantile(ht_data, c(0.25,0.50,0.75), na.rm=TRUE)
+
+#add to data.frame
+df_htplot<- data.frame(value=htplot_quant, quantile=c("25th", "50th","75th"))
+
+#import the library
+library(ggplot2)
+
+#ggplot the histograms
+ggplot(data, aes(ht_data))+geom_histogram()+geom_vline(data = df_htplot, aes(xintercept=value,colour=quantile),show.legend = TRUE, linetype="dashed")+theme_classic()
+
+#visualize or plot the quantiles for the wt(x-axis)
+wtplot_quant<- quantile(wt_data, c(0.25,0.50,0.75), na.rm=TRUE)
+
+#add to data.frame
+df_wtplot<- data.frame(value=wtplot_quant, quantile=c("25th", "50th","75th"))
+
+#ggplot the histograms
+ggplot(data, aes(wt_data))+geom_histogram()+geom_vline(data = df_wtplot, aes(xintercept=value,colour=quantile),show.legend = TRUE, linetype="dashed")+theme_classic()
+
 #scatter plot works with the order x-axis, y-axis
 plot(wt_data,ht_data)
 #identify(wt_data,ht_data, labels = paste0(wt_data, ht_data))
